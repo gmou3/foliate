7a9df8760d9e21b770135a89a4114f4c37742485
gmou3 <gmou3@hotmail.com>
2023-10-27 13:47:00 +0300

    Increase annotation mark precision

--- src/book-viewer.js
+++ src/book-viewer.js
@@ -806,11 +806,10 @@ export const BookViewer = GObject.registerClass({
             title: _('Annotation deleted'),
             button_label: _('Undo'),
         }), { 'button-clicked': () => {
-                this.#data.addAnnotation(annotation).then(() => {
-                    this._navbar.loadMarks(this.#book, this.#data.annotations.export())
-                })
-            }
-        }))
+            this.#data.addAnnotation(annotation).then(() => {
+                this._navbar.loadMarks(this.#book, this.#data.annotations.export())
+            })
+        }}))
     }
     #showSelection({ type, value, text, lang, pos: { point, dir } }) {
         if (type === 'annotation') return new Promise(resolve => {
@@ -836,8 +835,7 @@ export const BookViewer = GObject.registerClass({
                     const annotation = this.#data.annotations.get(value)
                     this.#data.addAnnotation(annotation ?? {
                         value, text,
-                        location: this.#data.storage.get('progress')[0],
-                        total: this.#data.storage.get('progress')[1],
+                        location: JSON.stringify(this._navbar._progress_scale.get_value()),
                         color: this.highlight_color,
                         created: new Date().toISOString(),
                     }).then(() => {
